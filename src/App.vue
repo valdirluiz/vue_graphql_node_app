1d5ad83bbdd7de88fa221c8e49fb1fbfbb3bd501
valdir <valdir@AirValdir.local>
2019-10-19 10:30:02 -0300

    criação de componentes

--- src/App.vue
+++ src/App.vue
@@ -5,106 +5,9 @@
       <br />
       <h6 class="text-secondary">Gerador de nomes utilizando Vue.js, GraphQL e Node</h6>
     </div>
+
     <div id="main">
-      <div class="container">
-        <div class="row">
-          <div class="col-md">
-            <h5>
-              Prefixos
-              <span class="badge badge-info">{{ prefixes.length }}</span>
-            </h5>
-            <div class="card">
-              <div class="card-body">
-                <ul class="list-group">
-                  <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
-                    <div class="row">
-                      <div class="col-md">{{ prefix }}</div>
-                      <div class="col-md text-right">
-                        <button v-on:click="deletePrefix(prefix)" class="btn btn-danger btn-sm">
-                          <span class="fa fa-trash" />
-                        </button>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-                <br />
-                <div class="input-group">
-                  <input
-                    v-model="prefix"
-                    v-on:keyup.enter="addPrefix(prefix)"
-                    class="form-control"
-                    type="text"
-                    placeholder="Digite o prefixo"
-                  />
-                  <div class="input-group-append">
-                    <button v-on:click="addPrefix(prefix)" class="btn btn-info">
-                      <span class="fa fa-plus"></span>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md">
-            <h5>
-              Sufixos
-              <span class="badge badge-info">{{ sufixes.length }}</span>
-            </h5>
-            <div class="card">
-              <div class="card-body">
-                <ul class="list-group">
-                  <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
-                    <div class="row">
-                      <div class="col-md">{{ sufix }}</div>
-                      <div class="col-md text-right">
-                        <button v-on:click="deleteSufix(sufix)" class="btn btn-danger btn-sm">
-                          <span class="fa fa-trash" />
-                        </button>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-                <br />
-                <div class="input-group">
-                  <input
-                    v-on:keyup.enter="addSufix(prefix)"
-                    v-model="sufix"
-                    class="form-control"
-                    type="text"
-                    placeholder="Digite o sufixo"
-                  />
-                  <div class="input-group-append">
-                    <button v-on:click="addSufix(sufix)" class="btn btn-info">
-                      <span class="fa fa-plus"></span>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <br />
-        <h5>
-          Domínios
-          <span class="badge badge-info">{{ domains.length }}</span>
-        </h5>
-        <div class="card">
-          <div class="card-body">
-            <ul class="list-group">
-              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
-                <div class="row">
-                  <div class="col-md">{{ domain.name }}</div>
-                  <div class="col-md text-right">
-                    <a  v-bind:href="domain.checkout" target="_blank" class="btn btn-info">
-                      <span class="fa fa-shopping-cart" aria-hidden="true" />
-                    </a>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <AppDomainList></AppDomainList>
     </div>
   </div>
 </template>
@@ -112,51 +15,12 @@
 <script>
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import AppDomainList from "./components/AppDomainList";
 
 export default {
   name: "app",
-  data() {
-    return {
-      prefix: "",
-      sufix: "",
-      prefixes: ["Air", "Jet", "Flight"],
-      sufixes: ["Hub", "Station", "Mart"]
-    };
-  },
-  methods: {
-    addPrefix(prefix) {
-      this.prefixes.push(prefix);
-      this.prefix = "";
-    },
-    addSufix(sufix) {
-      this.sufixes.push(sufix);
-      this.sufix = "";
-    },
-    deletePrefix(prefix) {
-      this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
-    },
-    deleteSufix(sufix) {
-      this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
-    }
-  },
-  computed: {
-    domains() {
-      const domains = [];
-      for (const prefix of this.prefixes) {
-        for (const sufix of this.sufixes) {
-          const name = prefix + sufix;
-					const url = name.toLowerCase();
-					const checkout = `https://checkout.hostgator.com.br/?a=add&sld=${url}&tld=.com.br`;
-					const domain = {
-						name,
-						url,
-						checkout
-					};
-					domains.push(domain);
-        }
-      }
-      return domains;
-    }
+  components: {
+    AppDomainList
   }
 };
 </script>
---
+++ src/components/AppItemList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h5>
+      {{title}}
+      <span class="badge badge-info">{{ items.length }}</span>
+    </h5>
+    <div class="card">
+      <div class="card-body">
+        <ul class="list-group">
+          <li class="list-group-item" v-for="item in items" v-bind:key="item">
+            <div class="row">
+              <div class="col-md">{{ item }}</div>
+              <div class="col-md text-right">
+                <button v-on:click="deleteItem(item)" class="btn btn-danger btn-sm">
+                  <span class="fa fa-trash" />
+                </button>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <br />
+        <div class="input-group">
+          <input
+            v-model="item"
+            v-on:keyup.enter="addItem(item)"
+            class="form-control"
+            type="text"
+            placeholder="Digite o item"
+          />
+          <div class="input-group-append">
+            <button v-on:click="addItem(item)" class="btn btn-info">
+              <span class="fa fa-plus"></span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppItemList",
+  props: ["title", "items"],
+  data() {
+    return {
+      item: ""
+    };
+  },
+  methods: {
+    addItem(item) {
+      this.$emit("addItem", item);
+      this.item = "";
+    },
+    deleteItem(item){
+      this.$emit("deleteItem",item);
+    }
+  }
+};
+</script>
+ 
+<style scoped>
+</style>
